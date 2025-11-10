8c1e44518a7a1f0760c327327800b0cf604ae5aa
Majuuuu22 <155262241+Majuuuu22@users.noreply.github.com>
2025-11-10 14:13:01 -0300

    atulizando

--- frontend/src/pages/Biblioteca.jsx
+++ frontend/src/pages/Biblioteca.jsx
@@ -10,7 +10,13 @@ const livrosFake = [
   { id: 6, nome: "Livro F", descricao: "Descrição do Livro F" },
   { id: 7, nome: "Livro G", descricao: "Descrição do Livro G" },
   { id: 8, nome: "Livro H", descricao: "Descrição do Livro H" },
-];
+  { id: 9, nome: "Livro J", descricao: "Descrição do Livro J" },
+  { id: 10, nome: "Livro K", descricao: "Descrição do Livro K" },
+  { id: 11, nome: "Livro L", descricao: "Descrição do Livro L" },
+  { id: 12, nome: "Livro M ", descricao: "Descrição do Livro M" },
+  { id: 13, nome: "Livro N", descricao: "Descrição do Livro N" },
+  { id: 14, nome: "Livro O", descricao: "Descrição do Livro O" },
+]
 
 const Biblioteca = ({ setFavoritos }) => {
   const [busca, setBusca] = useState("");
@@ -19,13 +25,9 @@ const Biblioteca = ({ setFavoritos }) => {
 
   const handleFavoritar = (livro) => {
     const jaFavoritado = favoritosLocal.find((f) => f.id === livro.id);
-    let novosFavoritos;
-
-    if (jaFavoritado) {
-      novosFavoritos = favoritosLocal.filter((f) => f.id !== livro.id);
-    } else {
-      novosFavoritos = [...favoritosLocal, livro];
-    }
+    const novosFavoritos = jaFavoritado
+      ? favoritosLocal.filter((f) => f.id !== livro.id)
+      : [...favoritosLocal, livro];
 
     setFavoritosLocal(novosFavoritos);
     setFavoritos(novosFavoritos);
@@ -49,14 +51,14 @@ const Biblioteca = ({ setFavoritos }) => {
       <div className="busca-container">
         <input
           type="text"
-          placeholder="buscar por livros..."
+          placeholder="Buscar por livros..."
           value={busca}
           onChange={(e) => setBusca(e.target.value)}
         />
         <button>Buscar</button>
       </div>
 
-      {/* GRID DE LIVROS (2 FILEIRAS) */}
+      {/* GRID DE LIVROS */}
       <div className="livros-grid">
         {livrosFiltrados.map((livro) => (
           <div
@@ -64,7 +66,7 @@ const Biblioteca = ({ setFavoritos }) => {
             className="livro-card"
             onClick={() => setLivroSelecionado(livro)}
           >
-            <div className="livro-imagem">ft do livro</div>
+            <div className="livro-imagem">Capa</div>
             <p>{livro.nome}</p>
             <button
               className={`favorito-btn ${
@@ -83,7 +85,10 @@ const Biblioteca = ({ setFavoritos }) => {
 
       {/* MODAL DE DESCRIÇÃO */}
       {livroSelecionado && (
-        <div className="descricao-modal" onClick={() => setLivroSelecionado(null)}>
+        <div
+          className="descricao-modal"
+          onClick={() => setLivroSelecionado(null)}
+        >
           <div className="descricao-box" onClick={(e) => e.stopPropagation()}>
             <h3>{livroSelecionado.nome}</h3>
             <p>{livroSelecionado.descricao}</p>
